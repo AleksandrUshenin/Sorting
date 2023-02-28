bb9ac4eacbe3dad4ec80dc7da326be740860e827
Aleksandr Ushenin <sasha0625@mail.ru>
2023-02-28 18:27:48 +0300

    добавлено левостороннее красно-черное дерево

---
+++ SorterMethods/SorterMethods/BinatyThree.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SorterMethods
+{
+    public class BinatyThree<T>
+    {
+        private Node Root;
+        private Comparer<T> _Comparer;
+
+        public BinatyThree()
+        { }
+        public BinatyThree(Comparer<T> comparer)
+        {
+            _Comparer = comparer;
+        }
+
+        /// <summary>
+        /// добавление
+        /// </summary>
+        /// <param name="value">значение</param>
+        /// <returns>результат добавления</returns>
+        public bool Add(T value)
+        {
+            if (Root != null)
+            {
+                bool result = AddNode(Root, value);
+                Root = Rebalance(Root);
+                Root.Color = ColorNode.Black;
+                return result;
+            }
+            else
+            {
+                Root = new Node(value);
+                Root.Color = ColorNode.Black;
+                return true;
+            }
+        }
+
+        private bool AddNode(Node node, T value)
+        {
+            if (Equals(node.Value, value))
+                return false;
+            else
+            {
+                if (Compare(node.Value, value))
+                {
+                    if (node.LeftChild != null)
+                    {
+                        bool result = AddNode(node.LeftChild, value);
+                        node.LeftChild = Rebalance(node.LeftChild);
+                        return result;
+                    }
+                    else
+                    {
+                        node.LeftChild = new Node(value);
+                        node.LeftChild.Color = ColorNode.Red;
+                        return true;
+                    }
+                }
+                else 
+                {
+                    if (node.RightChild != null)
+                    {
+                        bool result = AddNode(node.RightChild, value);
+                        node.RightChild = Rebalance(node.RightChild);
+                        return result;
+                    }
+                    else
+                    {
+                        node.RightChild = new Node(value);
+                        node.Color = ColorNode.Red;
+                        return true;
+                    }
+                }
+            }
+        }
+        private Node Rebalance(Node node)
+        {
+            Node result = node;
+            bool needRebalance;
+            do
+            {
+                needRebalance = false;
+                if (result.RightChild != null && result.RightChild.Color == ColorNode.Red &&
+                    (result.LeftChild == null || result.LeftChild.Color == ColorNode.Black))
+                {
+                    needRebalance = true;
+                    result = RightSwap(result);
+                }
+                if (result.LeftChild != null && result.LeftChild.Color == ColorNode.Red &&
+                    result.LeftChild.LeftChild != null && result.LeftChild.LeftChild.Color == ColorNode.Red)
+                {
+                    needRebalance = true;
+                    result = LeftSwap(result);
+                }
+                if (result.LeftChild != null && result.LeftChild.Color == ColorNode.Red &&
+                        result.RightChild != null && result.RightChild.Color == ColorNode.Red)
+                {
+                    needRebalance = true;
+                    ColorSwap(result);
+                }
+            }
+            while (needRebalance);
+            return result;
+        }
+        private Node RightSwap(Node node)
+        {
+            Node rightChild = node.RightChild;
+            Node betweenChild = node.LeftChild;
+            rightChild.LeftChild = node;
+            node.RightChild = betweenChild;
+            rightChild.Color = node.Color;
+            node.Color = ColorNode.Red;
+            return rightChild;
+        }
+        private Node LeftSwap(Node node)
+        {
+            Node leftChild = node.LeftChild;
+            Node betweenChild = node.RightChild;
+            leftChild.RightChild = node;
+            node.LeftChild = betweenChild;
+            leftChild.Color = node.Color;
+            node.Color = ColorNode.Red;
+            return leftChild;
+        }
+        private void ColorSwap(Node node)
+        {
+            node.RightChild.Color = ColorNode.Black;
+            node.LeftChild.Color = ColorNode.Black;
+            node.Color = ColorNode.Red;
+        }
+
+        public class Node
+        {
+            public T Value { get; private set; }
+            public ColorNode Color;
+            public Node LeftChild;
+            public Node RightChild;
+
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+            public override string ToString()
+            {
+                return "{ " + Value.ToString() + " " + Color + " }";
+            }
+        }
+        private bool Equals(T o1, T o2)
+        {
+            if (_Comparer != null)
+            {
+                if (_Comparer.Compare(o1, o2) == 0)
+                    return true;
+                return false;
+            }
+            return o1.Equals(o2);
+        }
+        private bool Compare(T o1, T o2)
+        {
+            if (_Comparer != null)
+            {
+                if (_Comparer.Compare(o1, o2) > 0)
+                    return true;
+                return false;
+            }
+            return o1.Equals(o2);
+        }
+
+    }
+    public enum ColorNode
+    {
+        Red, Black
+    }
+}
--- SorterMethods/SorterMethods/Program.cs
+++ SorterMethods/SorterMethods/Program.cs
@@ -21,90 +21,71 @@ namespace SorterMethods
     {
         static void Main(string[] args)
         {
-            MyCompare myC = new MyCompare();
-            PiramidalSort<int> ps = new PiramidalSort<int>(myC);
-            ps.Add(5);
-            ps.Add(2);
-            ps.Add(1);
-            ps.Add(3);
-            ps.Add(8);
-            ps.Add(6);
-            ps.Add(9);
-            ps.Add(7);
-            ps.Add(0);
-            ps.Sorter();
+            Compare2 c2 = new Compare2();
+            BinatyThree<int> bin = new BinatyThree<int>(c2);
 
-            ps = new PiramidalSort<int>(myC);
-            Console.WriteLine("\nмоя сортировка");
-            //int iteration = 0;
-            int[] arrE = Massivs.Create(100000);
-
-            DateTime startTimerE = DateTime.Now;
-            ps.Add(arrE);
-            ps.Sorter();
-            DateTime endTimerE = DateTime.Now;
-
-            //Console.WriteLine(startTimerE.TimeOfDay);
-            //Console.WriteLine(endTimerE.TimeOfDay);
-            Console.WriteLine(endTimerE.TimeOfDay - startTimerE.TimeOfDay);
-
-
-            Console.ReadKey();
-            int[] arr = { 4, 5, 0, 1, 7, 8};
-            int iteration = 0;
-            //Massivs.Print(Sorter.HeapSort(arr, ref iteration), iteration);
-            //Massivs.Print(Sorter.QuickSort(arr, ref iteration), iteration);
-            //Massivs.Print(Sorter.BableSort(arr, ref iteration), iteration);
-            //Massivs.Print(Sorter.BableSort(Massivs.Create(10000), ref iteration), iteration);
-            Console.WriteLine("Сортировка массива длинной 100000");
-
-            Console.WriteLine("\nПузырковая сортировка");
-            iteration = 0;
-            arr = Massivs.Create(100000);
-            DateTime startTimer = DateTime.Now;
-            Sorter.BubbleSort(arr, ref iteration);
-            DateTime endTimer = DateTime.Now;
-            Console.WriteLine("количество итераций " + iteration);
-            Console.WriteLine(endTimer.TimeOfDay - startTimer.TimeOfDay);
-
-            Console.WriteLine("\nСортировка выбором");
-            arr = Massivs.Create(100000);
-            iteration = 0;
-            startTimer = DateTime.Now;
-            Sorter.SelectionSort(arr, ref iteration);
-            endTimer = DateTime.Now;
-            Console.WriteLine("Количество итераций " + iteration);
-            Console.WriteLine(endTimer.TimeOfDay - startTimer.TimeOfDay);
-
-            Console.WriteLine("\nСортировка вставками");
-            arr = Massivs.Create(100000);
-            iteration = 0;
-            startTimer = DateTime.Now;
-            Sorter.Inserts(arr, ref iteration);
-            endTimer = DateTime.Now;
-            Console.WriteLine("количество итераций " + iteration);
-            Console.WriteLine(endTimer.TimeOfDay - startTimer.TimeOfDay);
-
-            Console.WriteLine("\nБыстрая сортировка");
-            arr = Massivs.Create(100000);
-            iteration = 0;
-            startTimer = DateTime.Now;
-            Sorter.QuickSort(arr, ref iteration);
-            endTimer = DateTime.Now;
-            Console.WriteLine("Количество итераций " + iteration);
-            Console.WriteLine(endTimer.TimeOfDay - startTimer.TimeOfDay);
-
-            Console.WriteLine("\nСортировка кучей");
-            arr = Massivs.Create(100000);
-            iteration = 0;
-            startTimer = DateTime.Now;
-            Sorter.HeapSort(arr, ref iteration);
-            endTimer = DateTime.Now;
-            Console.WriteLine("количество итераций " + iteration);
-            Console.WriteLine(endTimer.TimeOfDay - startTimer.TimeOfDay);
-
+            bin.Add(5);
+            bin.Add(7);
+            bin.Add(10);
+            bin.Add(3);
+            bin.Add(6);
 
             Console.ReadKey();
         }
     }
+    class Compare2 : Comparer<int>
+    {
+        public override int Compare(int x, int y)
+        {
+            if (x > y)
+                return 1;
+            else if (x < y)
+                return -1;
+            return 0;
+        }
+    }
 }
+//Console.WriteLine("\nПузырковая сортировка");
+//            iteration = 0;
+//            arr = Massivs.Create(100000);
+//            DateTime startTimer = DateTime.Now;
+//            Sorter.BubbleSort(arr, ref iteration);
+//            DateTime endTimer = DateTime.Now;
+//            Console.WriteLine("количество итераций " + iteration);
+//            Console.WriteLine(endTimer.TimeOfDay - startTimer.TimeOfDay);
+
+//            Console.WriteLine("\nСортировка выбором");
+//            arr = Massivs.Create(100000);
+//            iteration = 0;
+//            startTimer = DateTime.Now;
+//            Sorter.SelectionSort(arr, ref iteration);
+//            endTimer = DateTime.Now;
+//            Console.WriteLine("Количество итераций " + iteration);
+//            Console.WriteLine(endTimer.TimeOfDay - startTimer.TimeOfDay);
+
+//            Console.WriteLine("\nСортировка вставками");
+//            arr = Massivs.Create(100000);
+//            iteration = 0;
+//            startTimer = DateTime.Now;
+//            Sorter.Inserts(arr, ref iteration);
+//            endTimer = DateTime.Now;
+//            Console.WriteLine("количество итераций " + iteration);
+//            Console.WriteLine(endTimer.TimeOfDay - startTimer.TimeOfDay);
+
+//            Console.WriteLine("\nБыстрая сортировка");
+//            arr = Massivs.Create(100000);
+//            iteration = 0;
+//            startTimer = DateTime.Now;
+//            Sorter.QuickSort(arr, ref iteration);
+//            endTimer = DateTime.Now;
+//            Console.WriteLine("Количество итераций " + iteration);
+//            Console.WriteLine(endTimer.TimeOfDay - startTimer.TimeOfDay);
+
+//            Console.WriteLine("\nСортировка кучей");
+//            arr = Massivs.Create(100000);
+//            iteration = 0;
+//            startTimer = DateTime.Now;
+//            Sorter.HeapSort(arr, ref iteration);
+//            endTimer = DateTime.Now;
+//            Console.WriteLine("количество итераций " + iteration);
+//            Console.WriteLine(endTimer.TimeOfDay - startTimer.TimeOfDay);
